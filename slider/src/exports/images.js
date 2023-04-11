96146f7dcd035ef074530cb8b1b1fdf1925feb14
Uriens <ignaciocstlls@gmail.com>
2023-04-11 11:38:22 +0200

    first commit

---
+++ slider/src/exports/images.js
@@ -0,0 +1,8 @@
+import image1 from '../assets/1.jpg.jpg'
+import image2 from '../assets/2.jpg.jpg'
+import image3 from '../assets/3.jpg.jpg'
+import image4 from '../assets/4.jpg.jpg'
+import image5 from '../assets/5.jpg.jpg'
+import image6 from '../assets/6.jpg.jpg'
+
+export default [image1,image2,image3,image4,image5,image6]
